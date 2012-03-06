44e98cceff1c0c082f237c530e61d35956ba91b2
Elias da Rosa <elias@eliasdarosa.com.br>
2012-03-06 14:04:03 -0300

    BW - Corrigido erro de escrita no SQL

--- install/sql/2.sql.php
+++ install/sql/2.sql.php
@@ -1,7 +1,7 @@
 -- <? defined('BW') or die("Acesso negado!"); ?>
 
 --
-ALTER TABLE `bw_versao` CHANGE `bw_1` `bw_2` INT(1) NOT NULL
+ALTER TABLE `bw_versao` CHANGE `bw_1` `bw_2` INT(1) NOT NULL;
 
 --
 INSERT INTO `bw_configuracoes` (`var`, `value`, `default`, `tipo`, `params`, `titulo`, `protegido`, `oculto`, `desc`) 
