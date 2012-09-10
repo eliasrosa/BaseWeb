1cf3760d4e50c038e0486b2611ae4e13b070f011
Elias da Rosa <elias@eliasdarosa.com.br>
2012-09-10 10:42:49 -0300

    BW - Removido o sinal 'arroba' (@) dos requires no index

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ define('BW', 1);
 define('DS', DIRECTORY_SEPARATOR);
 define('BW_PATH', dirname(__FILE__));
 
-@require_once(BW_PATH .DS. 'config.php');
-@require_once(BW_PATH .DS. 'libraries' .DS. 'defines.php');
-@require_once(BW_PATH .DS. 'libraries' .DS. 'loader.php');
+require_once(BW_PATH .DS. 'config.php');
+require_once(BW_PATH .DS. 'libraries' .DS. 'defines.php');
+require_once(BW_PATH .DS. 'libraries' .DS. 'loader.php');
 
 // inicia a mágica!!!
 bwCore::init();
